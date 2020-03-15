b0a5596c75fa74bdafe5915482377ade419bd6b5
MadRavingMan <48089175+MadRavingMan@users.noreply.github.com>
2020-03-15 22:15:42 +0200

    added modal
    added modal

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,15 +1,27 @@
 // TODO : styling
-// TODO : Modal for alert
 
 <template>
   <div :id="dropdownName + '_dropdown'">
-    <b-dropdown size="lg" variant="secondary text-dark" offset="-22" @hide="validateChecks()">
+    <b-dropdown
+      size="lg"
+      variant="text-dark"
+      offset="-26"
+      @hide="validateChecks()"
+      class="dropdown-btn"
+      menu-class="shadow-sm"
+    >
       <template v-slot:button-content>
         {{ dropdownName }}
         <span class="marked-number">{{ markedItems }}</span>
       </template>
-      <b-dropdown-text v-for="item in dropListItems" :key="item">
-        <p-check class="p-svg p-curve" color="warning" @change="checkboxsCounter()">
+      <b-dropdown-text>
+        <p-check
+          v-for="item in dropListItems"
+          :key="item"
+          class="p-svg p-curve"
+          color="warning"
+          @change="checkboxsCounter()"
+        >
           <svg slot="extra" class="svg svg-icon" viewBox="0 0 20 20">
             <path
               d="M7.629,14.566c0.125,0.125,0.291,0.188,0.456,0.188c0.164,0,0.329-0.062,0.456-0.188l8.219-8.221c0.252-0.252,0.252-0.659,0-0.911c-0.252-0.252-0.659-0.252-0.911,0l-7.764,7.763L4.152,9.267c-0.252-0.251-0.66-0.251-0.911,0c-0.252,0.252-0.252,0.66,0,0.911L7.629,14.566z"
@@ -20,6 +32,11 @@
         </p-check>
       </b-dropdown-text>
     </b-dropdown>
+
+    <b-modal ref="warning-modal" hide-header hide-footer>
+      <h3>Please, choose atleast one item.</h3>
+      <b-button class="mt-8" block @click="hideModal()">Close</b-button>
+    </b-modal>
   </div>
 </template>
 
@@ -44,9 +61,15 @@ export default {
         "input[type='checkbox']:checked"
       ).length;
     },
+    showModal() {
+      this.$refs["warning-modal"].show();
+    },
+    hideModal() {
+      this.$refs["warning-modal"].hide();
+    },
     validateChecks() {
       if (!this.isExpanded && this.markedItems === 0) {
-        alert("Please, pick one item from " + this.dropdownName + " dropdown");
+        this.showModal();
       }
     }
   }
@@ -54,9 +77,11 @@ export default {
 </script>
 
 <style scoped>
-.dropdownbtn {
-  background-color: lightgray !important;
+.dropdown-btn {
+  background-color: lightgray;
+  border-radius: 5px;
 }
+
 .marked-number {
   background: gray;
   border-radius: 10px;
